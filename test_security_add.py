c848867862184fe6194ec51ab471ec07add6042e
Finift <finift@list.ru>
2023-01-10 21:50:07 +0300

    Tests and files for them

--- test_security_add.py
+++ test_security_add.py
@@ -6,17 +6,19 @@ shutil.copyfile('securities.txt', 'sec_add.txt')
 #Добавляем бумагу
 with open('sec_add.txt', mode='a') as sec_add:
     sec_add.write('BBG000B9Y9C7')
-#сравнение содержимого файлов
-data1 = open('securities.txt', 'r').readlines()
-data2 = open('sec_add.txt', 'r').readlines()
-output = []
-for item in data2:
-    if item not in data1:
-        output.append(item)
+#сравнение содержимого файлов для построчного сравнения
+# data1 = open('securities.txt', 'r').readlines()
+# data2 = open('sec_add.txt', 'r').readlines()
+# output = []
+# for item in data2:
+#     if item not in data1:
+#         output.append(item)
 
 
+#Просто проверка, что файл добавлен
 def test_comparison():
-    assert 'BBG000B9Y9C7' in output, "Item wasn't added"
+    data = open('sec_add.txt', 'r').readlines()
+    assert 'BBG000B9Y9C7' in data, "Item wasn't added"
 #
 # #Удаление всех данных из файла:
 # f = open('sec_add.txt', 'r+')
