2fe4097e504bd0b6064021662c05f6f3b71173b2
emamedova <finift@list.ru>
2023-02-13 16:12:23 +0300

    added new method

--- training/factorial.py
+++ training/factorial.py
@@ -1,8 +1,26 @@
-n = int(input())
+from _datetime import datetime
 
+# def factorial(n):
+#     fact = n
+#     for i in range(1, (n - 1)):
+#         fact = fact * (n - i)
+#     return fact
+#
+#
+# n = int(input())
+# start = datetime.now()
+# print(factorial(n))
+# end = datetime.now()
+# print((end - start).total_seconds() * 10**3)
+
+# В два раза быстрее, но n до 995!
 def factorial(n):
-    fact = n
-    for i in range(1, (n - 1)):
-        fact = fact * (n - i)
-    return fact
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+n = int(input())
+start = datetime.now()
 print(factorial(n))
+end = datetime.now()
+print((end - start).total_seconds() * 10**3)
